0c67d33a421f66cdcaaa1c63637dcd35f371ed1e
abhk943 <abhk943@gmail.com>
2021-06-11 21:11:16 +0530

    firebase login using provider stream

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_andy_2/providers/settings_provider.dart';
 import 'package:provider_andy_2/screens/home.dart';
+import 'package:provider_andy_2/services/firestore_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'providers/report.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,8 +18,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => SettingsProvider(), //ADD PROVIDER,
+    final FirestoreService _db = FirestoreService();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (BuildContext context) => SettingsProvider()),
+        StreamProvider<List<Report>>(
+          create: (BuildContext context) => _db.getReports(),
+          initialData: [
+            Report(-4, "Red", "Toko", "2021-06-08T19:02:49.657496")
+          ],
+        )
+      ],
+      //ADD PROVIDER,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'ANDY JULOW CHANGE NOTIFIER',
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,19 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_andy_2/providers/report.dart';
 import 'package:provider_andy_2/screens/settings.dart';
+import 'package:provider_andy_2/services/firestore_service.dart';
 
+// import '';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var reports = Provider.of<List<Report>>(context);
+    // print(reports.toString());
+    FirestoreService _db = FirestoreService();
     return Scaffold(
       appBar: AppBar(
         title: Text("WAXAPP"),
@@ -18,6 +26,25 @@ class Home extends StatelessWidget {
             icon: Icon(Icons.settings),
           ),
         ],
+      ),
+      // body: ListView.builder(
+      //   itemCount: reports.length,
+      //   itemBuilder: (context, index) {
+      //     Report report = reports[index];
+      //     return ListTile(
+      //       leading: Text(report.temp.toString()),
+      //       title: Text(report.wax),
+      //       subtitle: Text(report.line),
+      //       trailing: Text(formatDate(
+      //           DateTime.parse(report.timeStamp), [h, ":", nn, " ", am])),
+      //     );
+      //   },
+      // ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _db.addReport();
+        },
       ),
     );
   }
